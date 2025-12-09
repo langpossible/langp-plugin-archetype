e640ecee5771a64453619ab3ea046d4c7e3d595b
gengxiankun <gengxiankun@vn.com>
2025-12-09 15:54:24 +0800

    feat(/): 结构调整

---
+++ src/main/resources/archetype-resources/src/main/java/provider/__Name__Plugin.java
@@ -0,0 +1,11 @@
+package ${package};
+
+import java.util.Map;
+
+public class PluginTool {
+
+    public Boolean validate(Map<String, Object> credentials) {
+        return Boolean.FALSE;
+    }
+
+}
